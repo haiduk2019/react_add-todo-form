a8c241bc530c0d8fb677c1d2276187a856033678
Iryna Haiduk <$ git config --global user.name Iryna>
2021-04-11 21:22:17 +0300

    Updates

---
+++ precache-manifest.424c7b025dcd5e4cad591c4fd74d1296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97047491e1591e7847a277a24c4cbff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4008d512c752f99924af",
+    "url": "./static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "1fddd2aa37115b8d97bb",
+    "url": "./static/js/2.e8cc6336.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8cc6336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4008d512c752f99924af",
+    "url": "./static/js/main.3ab06845.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.424c7b025dcd5e4cad591c4fd74d1296.js"
 );
 
 self.addEventListener('message', (event) => {
